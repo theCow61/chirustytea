8168a52146525acca3acbd017004a6e7756b4f14
theCow61 <zanesalti@gmail.com>
2021-04-17 19:36:52 -0500

    lul

--- src/bank.rs
+++ src/bank.rs
@@ -1,15 +1,15 @@
 use super::Commandment;
 use serde_json::{json /*to_string_pretty*/, Value as JsonValue};
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
-#[derive(serde::Serialize, serde::Deserialize)]
+//#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Bank {
     // username: String,
     // balance: f64,
     // tele_user_executer: String,
-    pub json_object: JsonValue,
+    json_object: JsonValue,
     db_path: String,
-    pub msg_info: HashMap<String, String>,
+    //pub msg_info: HashMap<String, String>,
 }
 
 impl Bank {
@@ -19,12 +19,12 @@ impl Bank {
         let contents =
             std::fs::read_to_string(std::env::var("STAT_PATH").expect("Env not set..."))?;
         let res = serde_json::from_str(&contents)?;
-        let msg_info = HashMap::new();
+        //let msg_info = HashMap::new();
         Ok(Bank {
             // tele_user_executer: bruha,
             json_object: res,
             db_path: std::env::var("STAT_PATH").unwrap(),
-            msg_info,
+            //msg_info,
         })
         // match res {
         //     Ok(resa) => {
@@ -41,23 +41,26 @@ impl Bank {
         //         username: "lul".to_owned(),
         //         balance: 3f64,
     }
-    pub async fn get_balance(&mut self, commandent: &Commandment) -> Result<u64, std::io::Error> {
+    pub async fn get_balance(
+        &mut self,
+        commandent: &Commandment<'_>,
+    ) -> Result<u64, std::io::Error> {
         // if self.json_object["CowSheckles"][commandent.cap_map["Executer"]] == JsonValue::Null {
         //     self.json_object["CowSheckles"][commandent.cap_map["Executer"]] = json!(0);
         //     let new_contents = serde_json::to_string_pretty(&self.json_object)?;
         //     tokio::fs::write(&self.db_path, new_contents).await?;
         //     return Ok(0)
         // }
-        self.null_nullifier(&commandent.cap_map["Executer"]).await?;
-        let balance: u64 = self.json_object["CowSheckles"][&commandent.cap_map["Executer"]]
-            .as_u64()
-            .unwrap()
-            .to_owned();
+        let user = format!("@{}", commandent.cap_map["Executer"]);
+        let user = user.as_str();
+        self.null_nullifier(user).await?;
+        let balance: u64 = self.json_object["CowSheckles"][user].as_u64().unwrap();
+        //.to_owned();
         Ok(balance)
     }
     pub async fn transfer(
         &mut self,
-        commandent: &Commandment,
+        commandent: &Commandment<'_>,
         amount: u64,
     ) -> Result<Option<()>, Box<dyn std::error::Error>> {
         // if self.json_object["CowSheckles"][&commandent.cap_map["Executer"]] == JsonValue::Null {
@@ -67,41 +70,33 @@ impl Bank {
         //     return Ok(None)
         // }
         // if self.json_object["CowSheckles"][&commandent.cap_map[""]]
-        self.null_nullifier(&commandent.cap_map["Executer"]).await?;
-        self.null_nullifier(&commandent.cap_map["Recep"]).await?;
-        if self.json_object["CowSheckles"][&commandent.cap_map["Executer"]]
-            .as_u64()
-            .unwrap()
-            < amount
-        {
+        let user = format!("@{}", commandent.cap_map["Executer"]);
+        let user = user.as_str();
+        self.null_nullifier(user).await?;
+        self.null_nullifier(commandent.cap_map["Recep"]).await?;
+        if self.json_object["CowSheckles"][user].as_u64().unwrap() < amount {
             return Ok(None);
         }
-        if self.json_object["CowSheckles"][&commandent.cap_map["Executer"]]
-            .as_u64()
-            .unwrap()
-            >= amount
-        {
+        if self.json_object["CowSheckles"][user].as_u64().unwrap() >= amount {
             let to_inc = self.json_object["CowSheckles"][&commandent.cap_map["Recep"]]
                 .as_u64()
                 .unwrap()
                 + amount;
-            let to_dec = self.json_object["CowSheckles"][&commandent.cap_map["Executer"]]
-                .as_u64()
-                .unwrap()
-                - amount;
+            let to_dec = self.json_object["CowSheckles"][user].as_u64().unwrap() - amount;
             self.json_object["CowSheckles"][&commandent.cap_map["Recep"]] = json!(to_inc);
-            self.json_object["CowSheckles"][&commandent.cap_map["Executer"]] = json!(to_dec);
+            self.json_object["CowSheckles"][user] = json!(to_dec);
             let new_contents = serde_json::to_string_pretty(&self.json_object)?;
-            tokio::fs::write(&self.db_path, new_contents).await?;
+            //tokio::fs::write(&self.db_path, new_contents).await?;
+            async_std::fs::write(&self.db_path, new_contents).await?;
             return Ok(Some(()));
         }
         Ok(None)
     }
-    async fn null_nullifier(&mut self, test_if_null: &str) -> Result<(), tokio::io::Error> {
+    async fn null_nullifier(&mut self, test_if_null: &str) -> Result<(), async_std::io::Error> {
         if self.json_object["CowSheckles"][test_if_null] == JsonValue::Null {
             self.json_object["CowSheckles"][test_if_null] = json!(0);
             let new_contents = serde_json::to_string_pretty(&self.json_object)?;
-            tokio::fs::write(&self.db_path, new_contents).await?;
+            async_std::fs::write(&self.db_path, new_contents).await?;
         }
         Ok(())
     }
